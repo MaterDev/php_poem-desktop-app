7ff5dd49284a0161018c53d842765a1bbbbee2b8
Key Clark <aryeh.the.lion@gmail.com>
2024-12-11 15:30:22 -0600

    windows are draggable

--- resources/views/poems/index.blade.php
+++ resources/views/poems/index.blade.php
@@ -28,7 +28,39 @@
     </div>
 
     <script>
+        // Basic dragging functionality
+        document.querySelectorAll('.window').forEach(window => {
+            const title = window.querySelector('.window-title');
+            let isDragging = false;
+            let currentX;
+            let currentY;
+            let initialX;
+            let initialY;
 
+            title.addEventListener('mousedown', startDragging);
+            document.addEventListener('mousemove', drag);
+            document.addEventListener('mouseup', stopDragging);
+
+            function startDragging(event) {
+                isDragging = true;
+                initialX = event.clientX - window.offsetLeft;
+                initialY = event.clientY - window.offsetTop;
+            }
+
+            function drag(event) {
+                if (isDragging) {
+                    event.preventDefault();
+                    currentX = event.clientX - initialX;
+                    currentY = event.clientY - initialY;
+                    window.style.left = `${currentX}px`;
+                    window.style.top = `${currentY}px`;
+                }
+            }
+
+            function stopDragging() {
+                isDragging = false;
+            }
+        })
     </script>
 
 
