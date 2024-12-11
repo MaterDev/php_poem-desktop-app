e1f9face042a43c77b5cfa4a83ebac012340778d
Key Clark <aryeh.the.lion@gmail.com>
2024-12-11 13:51:23 -0600

    create poetry model for db

--- database/migrations/2024_12_11_074738_create_poems_table.php
+++ database/migrations/2024_12_11_074738_create_poems_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('poems', function (Blueprint $table) {
             $table->id();
+            $table->string('title');    
+            $table ->text('content');
+            $table->string('icon_path')->nullable();
+            $table->integer('position_x')->default(0);
+            $table->integer('position_y')->default(0);
             $table->timestamps();
         });
     }
