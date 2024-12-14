b2c12ab38258f0ad41c320e30d3bf8843cb96ffe
Key Clark <aryeh.the.lion@gmail.com>
2024-12-13 18:52:37 -0600

    fix logo to render properly

--- resources/views/poems/index.blade.php
+++ resources/views/poems/index.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div id="menu-bar" class="menu-bar">
-        <img class="apple-menu" alt="logo" src="{{ url('resources/images/logo.png') }}">
+        <img class="apple-menu" src="{{ url('/resources/images/logo.png') }}" alt="Apple Menu">
         <span>File</span>
     </div>
 
